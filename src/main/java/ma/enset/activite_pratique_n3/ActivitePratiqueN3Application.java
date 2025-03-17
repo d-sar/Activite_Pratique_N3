932bf3b39c7c9f907964c401f69d5bc83549e88d
d-sar <damrisara1@gmail.com>
2025-03-17 09:35:12 +0000

    Initialisation du projet

---
+++ src/main/java/ma/enset/activite_pratique_n3/ActivitePratiqueN3Application.java
@@ -0,0 +1,13 @@
+package ma.enset.activite_pratique_n3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitePratiqueN3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitePratiqueN3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/activite_pratique_n3/ActivitePratiqueN3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.activite_pratique_n3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitePratiqueN3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
